ea80dc99936e0c896167f7dc9ff8fe203a54d1a4
tusharkulkarni <tusharkulkarni1990@gmail.com>
2016-02-11 00:11:31 -0500

    added edit and delete buttons for recipies

--- client/recipes/recipe.js
+++ client/recipes/recipe.js
@@ -1,5 +1,19 @@
+Template.Recipe.helpers({
+	updateRecipeId: function() {
+		return this._id;
+	}
+});
+
 Template.Recipe.events({
 	'click .toggle-menu': function(){
 		Meteor.call('toggleMenuItem', this._id,this.inMenu);
+	},
+	'click .fa-trash': function(){
+		Meteor.call('deleteRecipe', this._id)
+	},
+	'click .fa-pencil': function(){
+		//Session var is false if its not present, so no need to set default value for editMode. 
+		//When we do session.get it will return false.
+		Session.set('editMode', !Session.get('editMode'))
 	}
 })
