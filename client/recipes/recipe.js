6c8df6a9fe74c00c2012808fe7b0d037ec87e0b0
tusharkulkarni <tusharkulkarni1990@gmail.com>
2016-02-11 01:07:30 -0500

    completed tutorial

--- client/recipes/recipe.js
+++ client/recipes/recipe.js
@@ -1,6 +1,14 @@
+Template.Recipe.onCreated(function(){
+	this.editMode = new ReactiveVar(false);
+});
+
 Template.Recipe.helpers({
 	updateRecipeId: function() {
 		return this._id;
+	},
+	//return Reactive var for given template only
+	editMode: function(){
+		return Template.instance().editMode.get();
 	}
 });
 
@@ -11,9 +19,11 @@ Template.Recipe.events({
 	'click .fa-trash': function(){
 		Meteor.call('deleteRecipe', this._id)
 	},
-	'click .fa-pencil': function(){
+	//Reactive Var changes: add event , template as parameters
+	'click .fa-pencil': function(event, template){
 		//Session var is false if its not present, so no need to set default value for editMode. 
 		//When we do session.get it will return false.
-		Session.set('editMode', !Session.get('editMode'))
+		template.editMode.set(!template.editMode.get());
+		//Session.set('editMode', !Session.get('editMode'))
 	}
-})
+});
